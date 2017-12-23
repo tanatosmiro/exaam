c623979ca811a535dc506de91f4237e6899a5cff
tanatosmiro1 <vanya-gritsay@mail.ru>
2017-12-23 15:52:43 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// ;(function($){
+//     "use strict";
+//     $( document ).ready(function() {
+//         var teamSlider = $('.ba-slider');
+//         teamSlider.slick({
+//             slide: '.ba-slide',
+//             arrows: true,
+//             prevArrow: '.ba-slider-prev',
+//             nextArrow: '.ba-slider-next',
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             variableWidth: true,
+//             autoplay: true,
+//             autoplaySpeed: 1000,
+//             centerMode: true,
+//             // fade: true
+//         });
+//     });
+// })(jQuery);
